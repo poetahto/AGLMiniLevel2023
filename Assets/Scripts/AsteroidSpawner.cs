7429c494433165042ab798822123648b4c706a62
Daniel Walls <walls.dk@gmail.com>
2023-10-20 20:04:23 -0700

    experimenting with spawning strategies

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class AsteroidSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("How quickly the asteroids spawn, in asteroids-per-minute")]
+        private float spawnRate = 10.0f;
+
+        [SerializeField]
+        private GameObject asteroidPrefab;
+
+        [SerializeField]
+        private Transform spawnPosition;
+
+        [SerializeField]
+        [Tooltip("How far from the player can asteroids strike.")]
+        private float strikingRange = 5.0f;
+
+        [SerializeField]
+        private Transform strikingVisualizer;
+
+        private GameState _gameState;
+        private float _timeSinceSpawn;
+
+        private void OnValidate()
+        {
+            strikingVisualizer.localScale = new Vector3(strikingRange, strikingRange, strikingRange);
+        }
+
+        private void Start()
+        {
+            _gameState = FindAnyObjectByType<GameState>();
+            strikingVisualizer.localScale = new Vector3(strikingRange, strikingRange, strikingRange);
+        }
+
+        private void Update()
+        {
+            bool playerExists = _gameState.TryGetPlayer(out GameObject playerInstance);
+
+            if (playerExists)
+            {
+                // Update the visualizer transform
+                strikingVisualizer.transform.position = playerInstance.transform.position;
+                strikingVisualizer.forward = playerInstance.transform.up * -1;
+
+                _timeSinceSpawn += Time.deltaTime;
+
+                if (_timeSinceSpawn > 60.0f / spawnRate)
+                {
+                    // spawn an asteroid
+                    SpawnAsteroid(playerInstance.transform);
+                    _timeSinceSpawn = 0;
+                }
+            }
+
+            strikingVisualizer.gameObject.SetActive(playerExists);
+        }
+
+        private void SpawnAsteroid(Transform playerTransform)
+        {
+            Debug.Log("Spawned an asteroid.");
+
+            RaycastHit spawnPos = PhysicsUtil.GetRandomPointOnGroundNear(
+                -playerTransform.up,
+                playerTransform.position,
+                strikingRange
+            );
+
+            Instantiate(asteroidPrefab, spawnPos.point, Quaternion.identity, transform);
+        }
+    }
+}
---
+++ Assets/Scripts/PhysicsUtil.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    // Static generic helper methods for some physics stuff.
+    public static class PhysicsUtil
+    {
+        private static RaycastHit[] _hits = new RaycastHit[30];
+
+        // Calculates a random point on the ground, within some range of a transform.
+        public static RaycastHit GetRandomPointOnGroundNear(Vector3 down, Vector3 point, float radius)
+        {
+            Vector2 randomOffset2d = Random.insideUnitCircle * (radius * 2);
+            Vector3 randomOffset = new Vector3(randomOffset2d.x, randomOffset2d.y, 0);
+            randomOffset = Quaternion.LookRotation(-down) * randomOffset;
+            Vector3 unsnappedRandomPosition = point + randomOffset;
+            return SnapToGround(down, unsnappedRandomPosition);
+        }
+
+        // Snaps a point to the closest collider, given some downwards direction.
+        public static RaycastHit SnapToGround(Vector3 down, Vector3 point)
+        {
+            Ray ray = new Ray(point, down);
+            int hits = Physics.RaycastNonAlloc(ray, _hits);
+
+            if (hits == 0)
+                return default;
+
+            // Find the closest result.
+            int resultIndex = 0;
+
+            for (int i = 0; i < hits; i++)
+            {
+                if (_hits[i].distance < _hits[resultIndex].distance)
+                    resultIndex = i;
+            }
+
+            return _hits[resultIndex];
+        }
+    }
+}
